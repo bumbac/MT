c97b04743058fcbb2550bafee7432c74af53d3d7
sutymate <sutymate@fit.cvut.cz>
2023-03-06 17:13:16 +0100

    change to testing

--- src/setup.py
+++ src/setup.py
@@ -7,13 +7,12 @@ from roommodel.utils.portrayal import agent_portrayal
 from roommodel.file_loader import FileLoader
 
 if __name__ == '__main__':
-    filename = "./maps/topology/long_left.txt"
+    filename = "./maps/topology/conflict.txt"
     filename = os.path.abspath(filename)
     fl = FileLoader(filename)
-    width, height = fl.dimensions()
-    grid = mesa.visualization.CanvasGrid(agent_portrayal, width, height, 800, 800)
+    canvas = fl.get_canvas()
     server = mesa.visualization.ModularServer(
-        RoomModel, [grid], "Room Model SETUP", {"filename": filename}
+        RoomModel, [canvas], "Room Model SETUP", {"filename": filename}
     )
     server.port = 8521  # The default
     server.launch()
