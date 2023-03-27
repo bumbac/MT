d5a24815377a1614b1342023d0610fa623079fda
sutymate <sutymate@fit.cvut.cz>
2023-03-27 17:37:28 +0200

    add graphs, new experiments

--- src/roommodel/datacollector.py
+++ src/roommodel/datacollector.py
@@ -30,8 +30,10 @@ class RoomDataCollector(mesa.DataCollector):
         self.__name__ = "RoomDataCollector " + str(self.model.generate_uid())
         self.data = {}
         self.experiments = [
-            ExperimentDistanceHeatmap(self.model),
-            ExperimentDistanceToLeader(self.model)
+            # ExperimentDistanceHeatmap(self.model),
+            # ExperimentDistanceToLeader(self.model),
+            # ExperimentGaps(self.model),
+            ExperimentIncorrectOrientation(self.model)
         ]
 
     def update(self):
@@ -55,194 +57,35 @@ class RoomDataCollector(mesa.DataCollector):
         name = str(event)
         data[step] = name
 
-    def distance_to_leader(self):
-        key = self.distance_to_leader.__name__
-        if key not in self.data:
-            self.data[key] = {uid: [] for uid in self.model.schedule.get_agents()}
-        data = self.data[key]
-        virtual_leader = self.model.virtual_leader
-
-        sff = self.model.sff["Follower"]
-        for agent in self.model.schedule.agents:
-            if agent.name.startswith("Follower"):
-                uid = agent.unique_id
-                d_to_leader = abs(sff[agent.pos[1], agent.pos[0]] - sff[virtual_leader.pos[1], virtual_leader.pos[0]])
-                data[uid].append(d_to_leader)
-        self.data[key] = data
-
-    def incorrect_orientation(self, uid, cells):
-        key = self.incorrect_orientation.__name__
+    def get_events(self):
+        key = self.events.__name__
         if key not in self.data:
             self.data[key] = {}
-        data = self.data[key]
-        if uid not in data:
-            data[uid] = {}
-        for choice in cells:
-            if choice not in data[uid]:
-                data[uid][choice] = []
-            data[uid][choice].append(cells[choice])
+        return self.data[key]
+
+    def incorrect_orientation(self, uid, cells):
+        experiment = None
+        for e in self.experiments:
+            if e.name == ExperimentIncorrectOrientation.__name__:
+                experiment = e
+        if experiment is None:
+            return
+        experiment.incorrect_orientation(uid, cells)
 
     def incorrect_orientation_selected(self, uid, choice_pos):
-        key = self.incorrect_orientation_selected.__name__
-        key2 = "incorrect_orientation_distance"
-        if key not in self.data:
-            self.data[key] = np.zeros_like(self.model.room)
-        if key2 not in self.data:
-            self.data[key2] = np.zeros((3, 20))
-
-        source_key = self.incorrect_orientation.__name__
-        data = self.data[source_key]
-        if uid in data:
-            for choice in data[uid]:
-                penalization, distance = data[uid][choice][-1]
-                leader, partner = choice_pos
-                leader_pos, _ = leader
-                partner_pos, _ = partner
-                if choice_pos == choice:
-                    self.data[key2][0, round(distance)] += 1
-                    self.data[key2][1, round(distance)] += penalization
-                    self.data[key][leader_pos[1], leader_pos[0]] += 1
-                    self.data[key][partner_pos[1], partner_pos[0]] += 1
-            self.data[key2][2, round(distance)] += 1
-
-    def save_incorrect_orientation(self):
-        key = self.incorrect_orientation_selected.__name__
-        key2 = "incorrect_orientation_distance"
-        data = self.data[key]
-        data2 = self.data[key2]
-        if os.path.exists(data_location+"incorrect_orientation.npy"):
-            incorrect_orientation_ctr_map = np.load(data_location+"incorrect_orientation.npy", allow_pickle=True)
-            data += incorrect_orientation_ctr_map
-        if os.path.exists(data_location+"incorrect_orientation_distance.npy"):
-            incorrect_orientation_distance = np.load(data_location+"incorrect_orientation_distance.npy", allow_pickle=True)
-            data2 += incorrect_orientation_distance
-
-        visualize = False
-        if visualize:
-            plt.figure(figsize=FIGSIZE)
-            plt.imshow(data)
-            plt.show(block=False)
-            plt.pause(0.1)
-            plt.figure(figsize=FIGSIZE)
-            plt.plot(data2[0]/data2[2])
-            plt.show(block=False)
-            plt.pause(0.1)
-            plt.figure(figsize=FIGSIZE)
-            plt.plot(data2[1]/data2[0])
-            plt.show(block=False)
-            plt.pause(0.1)
-
-        np.save(data_location+"incorrect_orientation.npy", data)
-        np.save(data_location+"incorrect_orientation_distance.npy", data2)
-
-    def gaps_between_agents(self):
-        key = self.gaps_between_agents.__name__
-        if key not in self.data:
-            self.data[key] = {uid: [] for uid in self.model.schedule.get_agents()}
-            self.data[key][-1] = True
-            del self.data[key][self.model.leader.unique_id]
-            del self.data[key][self.model.virtual_leader.unique_id]
-        if self.events.__name__ in self.data:
-            event_data = self.data[self.events.__name__]
-        else:
+        experiment = None
+        for e in self.experiments:
+            if e.name == ExperimentIncorrectOrientation.__name__:
+                experiment = e
+        if experiment is None:
             return
-        if len(event_data) < 1:
-            return
-        data = self.data[key]
-        if len(event_data) == 1:
-            ctr = 0
-            for agent in self.model.schedule.agents:
-                if agent.partner is not None:
-                    if agent.leader:
-                        uid = agent.unique_id
-                        if len(data[uid]) > 0:
-                            data[uid].append((agent.pos, agent.partner.pos, data[uid][0][2]))
-                        elif data[-1]:
-                            data[uid].append((agent.pos, agent.partner.pos, ctr))
-                        ctr += 1
-            data[-1] = False
-        self.data[key] = data
-
-    def visualize_gaps_between_agents(self):
-        key = self.gaps_between_agents.__name__
-        data = self.data[key]
-        del data[-1]
-        leader_uids = []
-        leader_ctr = []
-        for uid in data:
-            if len(data[uid]) > 0:
-                leader_uids.append(uid)
-                leader_ctr.append(data[uid][0][2])
-        leader_starts = {uid: [] for uid in leader_uids}
-        n_dist = len(leader_starts)
-        test_length = 20
-        for uid in leader_uids:
-            for item in data[uid]:
-                leader, partner, ctr = item
-                leader_starts[uid].append((leader[0]+partner[0])/2)
-        if os.path.exists(data_location+"gaps.npy"):
-            distances = list(np.load(data_location+"gaps.npy", allow_pickle=True))
-            distances = [list(a) for a in distances]
-        else:
-            distances = [[] for _ in range(n_dist)]
-        for i in range(test_length):
-            p = []
-            for uid in leader_starts:
-                if len(leader_starts[uid]) <= i:
-                    continue
-                else:
-                    p.append(leader_starts[uid][i])
-            p = sorted(p)
-            low_p = p[0]
-            for j, pp in enumerate(p):
-                distances[j].append(pp - low_p)
-        np.save(data_location+"gaps.npy", distances)
-
-    def boxplot_distance_to_leader(self):
-        key = self.distance_to_leader.__name__
-        data = self.data[key]
-        fig, ax = plt.subplots(figsize=FIGSIZE)
-        for uid in data:
-            if len(data[uid]) < 51:
-                continue
-            x_pos = data[uid][50]
-            if x_pos == 0:
-                continue
-            ax.boxplot(data[uid], positions=[x_pos], showfliers=False)
-            ax.scatter(x_pos, data[uid][0])
-        plt.show(block=False)
-        plt.pause(1)
-
-    def visual_distance_to_leader(self):
-        key = self.distance_to_leader.__name__
-        data = self.data[key]
-        del data[self.model.leader.unique_id]
-        del data[self.model.virtual_leader.unique_id]
-        plt.figure(figsize=FIGSIZE)
-        y = []
-        low_limit = 0
-        hi_limit = -1
-        smoothing_level = 5
-        for uid in data:
-            y.append(len(data[uid]))
-            data[uid] = rolling_avg(data[uid], smoothing_level)[low_limit:hi_limit]
-        y = minmax_norm(np.array(y))
-        cm = plt.get_cmap("viridis")
-        y = [cm(a) for a in y]
-        for uid in data:
-            data[uid] = np.array(data[uid])
-            plt.plot(data[uid], c=y.pop())
-        for xtick in [step for step in self.data[self.events.__name__]]:
-            plt.axvline(x=xtick)
-        plt.show(block=False)
-        plt.pause(0.1)
+        experiment.incorrect_orientation_selected(uid, choice_pos)
 
     def get_data(self):
         pass
     #     # self.boxplot_distance_to_leader()
     #     # self.visual_distance_to_leader()
     #     # self.visualize_distance_heatmap()
-    #     # self.visualize_gaps_between_agents()
     #     self.save_incorrect_orientation()
     #     return self.data
 
--- src/roommodel/experiment.py
+++ src/roommodel/experiment.py
@@ -1,19 +1,27 @@
+import os
+
 import mesa
 import numpy as np
 import pandas as pd
+import pickle
+
 import matplotlib
-import os
+
 matplotlib.use('tkagg')
 import matplotlib.pyplot as plt
 
 from .utils.constants import SFF_OBSTACLE, KS, KO, KD, GAMMA, OCCUPIED_CELL, EMPTY_CELL
 
-FIGSIZE = (20, 5)
+
+FIGSIZESQUARE = (8, 8)
+FIGSIZEWIDE = (20, 5)
+FIGSIZE4_3 = (8, 6)
+
 location = "./out/"
 
 
 def minmax_norm(arr):
-    return (arr - min(arr))/(max(arr) - min(arr))
+    return (arr - min(arr)) / (max(arr) - min(arr))
 
 
 def rolling_avg(x, N):
@@ -25,7 +33,7 @@ class Experiment:
     def __init__(self, model, name=None):
         self.model = model
         self.filename = str(model.filename).split(sep="/")[-1]
-        self.location = location+self.filename[:-4]
+        self.location = location + self.filename[:-4]
         self.data_location = self.location + "/data/"
         self.graphs_location = self.location + "/graphs/"
         if not os.path.isdir(self.location):
@@ -41,17 +49,19 @@ class Experiment:
         self.data = self.load()
         self.do_save = True
         self.do_show = False
+        self.figsize = FIGSIZE4_3
+
 
     def update(self):
         pass
 
     def save(self):
         key = 0
-        np.save(self.data_location+".npy", self.data[key])
+        np.save(self.data_location + ".npy", self.data[key])
 
     def load(self):
-        if os.path.exists(self.data_location+".npy"):
-            return {0: np.load(self.data_location+".npy", allow_pickle=True)}
+        if os.path.exists(self.data_location + ".npy"):
+            return {0: np.load(self.data_location + ".npy", allow_pickle=True)}
         else:
             return {}
 
@@ -77,11 +87,11 @@ class ExperimentDistanceHeatmap(Experiment):
         key = 0
         data = self.data[key]
         data = np.flip(data, axis=0)
-        fig, ax = plt.subplots(figsize=FIGSIZE)
+        fig, ax = plt.subplots(figsize=self.figsize)
         plt.title("Frequency of occupied cells")
         ax.imshow(data)
         if save or self.do_save:
-            plt.savefig(self.graphs_location+".png")
+            plt.savefig(self.graphs_location + ".png")
             plt.savefig(self.graphs_location + ".pdf")
         if show or self.do_show:
             plt.show(block=False)
@@ -117,7 +127,7 @@ class ExperimentDistanceToLeader(Experiment):
     def visualize(self, save=True, show=False):
         key = 0
         data = self.data[key]
-        fig, ax = plt.subplots(figsize=FIGSIZE)
+        fig, ax = plt.subplots(figsize=self.figsize)
         for uid in data:
             if len(data[uid]) < 51:
                 continue
@@ -139,7 +149,7 @@ class ExperimentDistanceToLeader(Experiment):
         data = self.data[key]
         del data[self.model.leader.unique_id]
         del data[self.model.virtual_leader.unique_id]
-        fig, ax = plt.subplots(figsize=FIGSIZE)
+        fig, ax = plt.subplots(figsize=self.figsize)
         y = []
         low_limit = 0
         hi_limit = -1
@@ -166,3 +176,219 @@ class ExperimentDistanceToLeader(Experiment):
             plt.show(block=False)
             plt.pause(1)
 
+
+class ExperimentGaps(Experiment):
+
+    def load(self):
+        return {}
+
+    def save(self):
+        pass
+
+    def update(self):
+        key = 0
+        if key not in self.data:
+            self.data[key] = {uid: [] for uid in self.model.schedule.get_agents()}
+            self.data[key][-1] = True
+            del self.data[key][self.model.leader.unique_id]
+            del self.data[key][self.model.virtual_leader.unique_id]
+        event_data = self.model.datacollector.get_events()
+        if len(event_data) < 1:
+            return
+        data = self.data[key]
+        if len(event_data) == 1:
+            ctr = 0
+            for agent in self.model.schedule.agents:
+                if agent.partner is not None:
+                    if agent.leader:
+                        uid = agent.unique_id
+                        if len(data[uid]) > 0:
+                            data[uid].append((agent.pos, agent.partner.pos, data[uid][0][2]))
+                        elif data[-1]:
+                            data[uid].append((agent.pos, agent.partner.pos, ctr))
+                        ctr += 1
+            data[-1] = False
+        self.data[key] = data
+
+    def finalize(self):
+        key = 0
+        data = self.data[key]
+        del data[-1]
+        leader_uids = []
+        leader_ctr = []
+        for uid in data:
+            if len(data[uid]) > 0:
+                leader_uids.append(uid)
+                leader_ctr.append(data[uid][0][2])
+        leader_starts = {uid: [] for uid in leader_uids}
+        n_dist = len(leader_starts)
+        test_length = 20
+        for uid in leader_uids:
+            for item in data[uid]:
+                leader, partner, ctr = item
+                leader_starts[uid].append((leader[0] + partner[0]) / 2)
+        if os.path.exists(self.data_location + ".dat"):
+            with open(self.data_location + ".dat", "rb") as f:
+                distances = pickle.load(f)
+        else:
+            distances = [[] for _ in range(n_dist)]
+        for i in range(test_length):
+            p = []
+            for uid in leader_starts:
+                if len(leader_starts[uid]) <= i:
+                    continue
+                else:
+                    p.append(leader_starts[uid][i])
+            p = sorted(p)
+            low_p = p[0]
+            for j, pp in enumerate(p):
+                distances[j].append(pp - low_p)
+        return distances
+
+    def visualize(self, save=True, show=False):
+        distances = self.finalize()
+        if self.do_save:
+            with open(self.data_location + ".dat", "wb") as f:
+                pickle.dump(distances, f)
+
+        fig, axs = plt.subplots(nrows=4, ncols=1, figsize=self.figsize, sharex="col")
+        for i, array in enumerate(distances):
+            axs[i].hist(array)
+        plt.xlabel("Distance to leader")
+        if save or self.do_save:
+            plt.savefig(self.graphs_location + ".png")
+            plt.savefig(self.graphs_location + ".pdf")
+        show = False
+        if show or self.do_show:
+            plt.show(block=False)
+            plt.pause(10)
+
+
+class ExperimentIncorrectOrientation(Experiment):
+    def __init__(self, model):
+        super().__init__(model)
+        self.do_save = False
+        self.do_show = False
+
+    def incorrect_orientation(self, uid, cells):
+        key3 = "incorrect_orientation"
+        if key3 not in self.data:
+            self.data[key3] = {}
+        data = self.data[key3]
+        if uid not in data:
+            data[uid] = {}
+        for choice in cells:
+            if choice not in data[uid]:
+                data[uid][choice] = []
+            data[uid][choice].append(cells[choice])
+        self.data[key3] = data
+
+    def incorrect_orientation_selected(self, uid, choice_pos):
+        key = "incorrect_orientation_selected"
+        key2 = "incorrect_orientation_distance"
+        test_length = 20
+        if key not in self.data:
+            self.data[key] = np.zeros_like(self.model.room)
+        if key2 not in self.data:
+            self.data[key2] = np.zeros((3, test_length))
+
+        key3 = "incorrect_orientation"
+        data = self.data[key3]
+        if uid in data:
+            for choice in data[uid]:
+                penalization, distance = data[uid][choice][-1]
+                leader, partner = choice_pos
+                leader_pos, _ = leader
+                partner_pos, _ = partner
+                if choice_pos == choice:
+                    self.data[key2][0, round(distance)] += 1
+                    self.data[key2][1, round(distance)] += penalization
+                    self.data[key][leader_pos[1], leader_pos[0]] += 1
+                    self.data[key][partner_pos[1], partner_pos[0]] += 1
+            self.data[key2][2, round(distance)] += 1
+
+    def load(self):
+        key = "incorrect_orientation_selected"
+        key2 = "incorrect_orientation_distance"
+        test_length = 20
+        data = np.zeros_like(self.model.room)
+        data2 = np.zeros((3, test_length))
+        if os.path.exists(self.data_location+"_selected.npy"):
+            incorrect_orientation_ctr_map = np.load(self.data_location+"_selected.npy", allow_pickle=True)
+            data += incorrect_orientation_ctr_map
+        if os.path.exists(self.data_location+"_distance.npy"):
+            incorrect_orientation_distance = np.load(self.data_location+"_distance.npy", allow_pickle=True)
+            data2 += incorrect_orientation_distance
+        d = {key: data,
+             key2: data2}
+        return d
+
+    def save(self):
+        key = "incorrect_orientation_selected"
+        key2 = "incorrect_orientation_distance"
+        data = self.data[key]
+        data2 = self.data[key2]
+        if os.path.exists(self.data_location+"_selected.npy"):
+            incorrect_orientation_ctr_map = np.load(self.data_location+"_selected.npy", allow_pickle=True)
+            data += incorrect_orientation_ctr_map
+        if os.path.exists(self.data_location+"_distance.npy"):
+            incorrect_orientation_distance = np.load(self.data_location+"_distance.npy", allow_pickle=True)
+            data2 += incorrect_orientation_distance
+
+        np.save(self.data_location+"_selected.npy", data)
+        np.save(self.data_location+"_distance.npy", data2)
+
+    def visualize(self, save=False, show=False):
+        key = "incorrect_orientation_selected"
+        key2 = "incorrect_orientation_distance"
+        data = self.data[key]
+        data2 = self.data[key2]
+
+        fig, ax = plt.subplots(figsize=self.figsize)
+        data = np.flip(data, axis=0)
+        ax.set_title("Frequency of Incorrect Orientation Maneuvers")
+        ax.imshow(data)
+        show = save = True
+        if save or self.do_save:
+            plt.savefig(self.graphs_location + "Heatmap.png")
+            plt.savefig(self.graphs_location + "Heatmap.pdf")
+        if show or self.do_show:
+            plt.show(block=False)
+            plt.pause(2)
+
+        fig, ax = plt.subplots(figsize=self.figsize)
+        ax.set_xlabel("Distance to Leader")
+        ax.set_ylabel("Penalization")
+        xticks = range(len(data2[1]))
+        ax.plot(xticks, data2[1] / data2[0])
+        plt.xticks(xticks)
+        if save or self.do_save:
+            plt.savefig(self.graphs_location + "Penalization.png")
+        if show or self.do_show:
+            plt.show(block=False)
+            plt.pause(2)
+
+        fig, ax = plt.subplots(figsize=self.figsize)
+        trailing_end = 0
+        total = data2[2][1:]
+        for i in range(len(total)):
+            if total[i] < 10:
+                trailing_end = i
+                break
+        total = total[:trailing_end]
+        norm_data = total / sum(total)
+        xticks = range(1, len(norm_data) + 1)
+        ax.scatter(y=norm_data, x=xticks, label="All maneuvers", alpha=0.5, color='orange')
+        hits = data2[0][1:trailing_end + 1]
+        ax.set_xlabel("Distance to Leader")
+        ax.set_ylabel("Ratio")
+        # ax.scatter(y=hits / total, x=xticks, label="Incorrect maneuvers", alpha=0.5, color='blue')
+        ax.plot(xticks, hits/total, label="Incorrect maneuvers")
+        plt.xticks(xticks)
+        plt.legend()
+
+        if save or self.do_save:
+            plt.savefig(self.graphs_location + "Ratio.png")
+        if show or self.do_show:
+            plt.show(block=False)
+            plt.pause(2)
